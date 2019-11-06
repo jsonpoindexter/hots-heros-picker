1f6a633ce23143710499a308f5ebd248186fdc6e
Jason Poindexter <poindexter.json@gmail.com>
2019-11-06 15:52:43 -0800

    Only append hero name to player if a hero is selected

--- src/components/Team/Players.vue
+++ src/components/Team/Players.vue
@@ -9,9 +9,21 @@
       <tr></tr>
       <tr v-for="(_, index) in 10" style="color: white;">
         <!-- Allow for 10 players to be on a team, but only show empty up to 5  -->
-        <td v-if="index <= 4|| redPlayers[index] && index > 4" style="background: red">{{ redPlayers[index] ? (redPlayers[index].name + ' - ' + $store.state.heros[redPlayers[index].selectedId].name) : 'empty' }}</td>
+        <td v-if="index <= 4 || (redPlayers[index] && index > 4)" style="background: red">
+          {{
+            redPlayers[index]
+              ? displayName(redPlayers[index])
+              : 'empty'
+          }}
+        </td>
         <td v-else></td>
-        <td v-if="index <= 4|| bluePlayers[index] && index > 4 " style="background: blue">{{ bluePlayers[index] ? (bluePlayers[index].name + ' - ' + $store.state.heros[bluePlayers[index].selectedId].name) : 'empty'}}</td>
+        <td v-if="index <= 4 || (bluePlayers[index] && index > 4)" style="background: blue">
+          {{
+            bluePlayers[index]
+              ? displayName(bluePlayers[index])
+              : 'empty'
+          }}
+        </td>
         <td v-else></td>
       </tr>
     </tbody>
@@ -23,6 +35,10 @@ import { Player, Team } from '@/store/types'
 
 @Component({})
 export default class Players extends Vue {
+  displayName(player: Player) {
+    return player.name + (player.selectedId !== null ? (' - ' + this.$store.state.heros[player.selectedId].name) : '')
+  }
+
   get players() {
     return [this.$store.state.user, ...this.$store.state.players]
   }
@@ -40,10 +56,12 @@ export default class Players extends Vue {
 </script>
 <style scoped>
 table {
-  width: 500px;
+  min-width: 700px;
   pointer-events: none;
 }
 td {
+  padding: 3px 8px ;
+  text-align: left;
   font-size: 26px;
   width: 50%;
 }
