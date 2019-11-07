206d2290652ce0a827a7cf79f057035052a4b495
Jason Poindexter <poindexter.json@gmail.com>
2019-11-06 23:42:17 -0800

    WIP Add banned table

--- src/components/Team/Banned.vue
+++ src/components/Team/Banned.vue
@@ -1,6 +1,16 @@
 <template>
   <table>
-      <thead></thead>
+    <thead>
+      <tr>
+        <th colspan="2">Banned Heros</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr></tr>
+        <tr v-for="id in $store.getters.bannedHeroIds">
+            <td>{{ $store.state.heros[id].name }}</td>
+        </tr>
+    </tbody>
   </table>
 </template>
 <script lang="ts">
--- src/components/Team/index.vue
+++ src/components/Team/index.vue
@@ -3,15 +3,20 @@
     <UsernameInput></UsernameInput>
     <SelectTeam />
     <Players></Players>
+    <Banned />
   </div>
+
+
+
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import SelectTeam from '@/components/Team/SelectTeam.vue'
 import UsernameInput from '@/components/Team/UsernameInput.vue'
 import Players from '@/components/Team/Players.vue'
+import Banned from '@/components/Team/Banned.vue'
 @Component({
-  components: {UsernameInput, SelectTeam, Players },
+  components: {Banned, UsernameInput, SelectTeam, Players },
 })
 export default class TeamPanel extends Vue {}
 </script>
